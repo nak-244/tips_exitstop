96cb33f46722eda22d0604c5e78620bebbe7dd12
nak-244 <t09024656860@gmail.com>
2019-11-27 18:03:10 +0900

    up

---
+++ sample3/assets/js/gyron.js
@@ -0,0 +1,14 @@
+
+  var __gyr = {
+    "config": {
+      "clientToken": "RDTV09I35N",
+      "site": "olp.co.jp"
+    }
+  };
+  (function() {
+    var ub = document.createElement('script');
+    ub.type = 'text/javascript'; ub.async = true;
+    ub.src = location.protocol + "//cmn.gyro-n.com/js/gyr.min.js";
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ub, s);
+  })();
